39332b87d7bd2b9076ce7bca18002c4086671884
Helen Marshall <marshallhelenm@gmail.com>
2019-07-01 02:27:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,10 +1,14 @@
 def monopoly
-	monopoly = {railroads: {}}
+	monopoly = {
+	  railroads: {}
+	}
 
 end
 
 def monopoly_with_second_tier
-	monopoly = {railroads: {pieces: 4, names: {}}}
+	monopoly = {
+	  railroads: {pieces: 4, names: {}, rent_in_dollars: {}}
+	}
 
 end
 
